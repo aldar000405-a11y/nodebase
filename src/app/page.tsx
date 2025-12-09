0c7ef8d46aca09ed33633082759449b82599b627
محمد سلطان <ALDAR000405@std.aldar.ac.ae>
2025-12-09 14:13:41 +0400

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center">
+      <Button variant="outline">
+        Click me
+      </Button>
+    </div>
+  );
+};
+
+export default Page;
